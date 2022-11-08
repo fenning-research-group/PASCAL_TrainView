f6db62100d93231290086087effa8118a9acffce
kcw <dwong1375@gmail.com>
2022-11-07 23:33:31 -0800

    add get_batch_o2log to crop the o2log for a batch

--- frgtrainview/analysis/o2log.py
+++ frgtrainview/analysis/o2log.py
@@ -1,5 +1,7 @@
 import datetime
 import pandas as pd
+import glob
+import os
 
 """
 Read from the o2 arduino running log and determine batch based on times
@@ -20,3 +22,28 @@ def slice_o2log(start_datetime, end_datetime):
     sliced = df[df['datetime'] >= start_datetime]
     sliced = sliced[sliced['datetime'] <= end_datetime]
     return sliced
+
+def get_batch_o2log(outputdir):
+    # find the .log file for timestamps of when batch started and ended
+    # start time is on first row, end time is on last row
+    output_logfile = glob.glob(os.path.join(outputdir, '*.log'))[0]
+    
+    first_line, last_line = None, None
+    with open(output_logfile) as file:
+        # not the most efficient since all lines are read in, but works for small logs
+        lines = file.readlines()
+        first_line, last_line = lines[0], lines[-1]
+
+    # now extract the times
+    # in format mm/dd/yyyy hh:mm:ss AM/PM
+    # day/time is 0-padded
+    format = "%m/%d/%Y %I:%M:%S %p"
+
+    start_time = ' '.join(first_line.split(' ')[:3])
+    end_time = ' '.join(last_line.split(' ')[:3])
+
+    start_time = datetime.datetime.strptime(start_time, format)
+    end_time = datetime.datetime.strptime(end_time, format)
+    print(start_time, end_time)
+
+    return slice_o2log(start_time, end_time)
