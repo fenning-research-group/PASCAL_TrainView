a42ad888e82969b5db6109bb1891887124044eab
kcw <dwong1375@gmail.com>
2022-11-07 23:34:51 -0800

    don't hardcode o2 log file used by slice_o2log

--- frgtrainview/analysis/o2log.py
+++ frgtrainview/analysis/o2log.py
@@ -10,7 +10,7 @@ Read from the o2 arduino running log and determine batch based on times
 
 LOG_FILE = 'PASCAL_ENV_2022-10-31.csv'
 
-def slice_o2log(start_datetime, end_datetime):
+def slice_o2log(start_datetime, end_datetime, o2log_file=LOG_FILE):
     # extract the rows in the log between two times
     # structure is datetime, oxygen_ppm, humidity_ppm
     # date is in format yyyy/mm/dd hh:mm:ss (24-hour time, single digits not 0-padded)
@@ -18,7 +18,7 @@ def slice_o2log(start_datetime, end_datetime):
 
     # note: this may get less efficient as the log gets bigger, since whole file is loaded
     # try using the log interval to smartly search instead
-    df = pd.read_csv(LOG_FILE, names=['datetime', 'oxygen_ppm', 'humidity_ppm'], parse_dates=[0])
+    df = pd.read_csv(o2log_file, names=['datetime', 'oxygen_ppm', 'humidity_ppm'], parse_dates=[0])
     sliced = df[df['datetime'] >= start_datetime]
     sliced = sliced[sliced['datetime'] <= end_datetime]
     return sliced
